30d3a86695652a26bb61a34e3ddd9465bccb49d7
Lanzerth <rvga1311@gmail.com>
2021-07-02 18:01:55 -0600

    Virtual assistant C#

---
+++ C#/Asistente Virtual/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace Asistente_Virtual
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.principal_button = new System.Windows.Forms.Button();
+            this.principal_panel1 = new Asistente_Virtual.principal_panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.principal_button);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 639);
+            this.panel1.TabIndex = 0;
+            // 
+            // principal_button
+            // 
+            this.principal_button.BackColor = System.Drawing.Color.LightCyan;
+            this.principal_button.Location = new System.Drawing.Point(61, 120);
+            this.principal_button.Name = "principal_button";
+            this.principal_button.Size = new System.Drawing.Size(104, 44);
+            this.principal_button.TabIndex = 0;
+            this.principal_button.Text = "button1";
+            this.principal_button.UseVisualStyleBackColor = false;
+            this.principal_button.Click += new System.EventHandler(this.principal_button_Click);
+            // 
+            // principal_panel1
+            // 
+            this.principal_panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.principal_panel1.Location = new System.Drawing.Point(256, 0);
+            this.principal_panel1.Name = "principal_panel1";
+            this.principal_panel1.Size = new System.Drawing.Size(806, 636);
+            this.principal_panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 639);
+            this.Controls.Add(this.principal_panel1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button principal_button;
+        private principal_panel principal_panel1;
+    }
+}
+
---
+++ C#/Asistente Virtual/principal_panel.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace Asistente_Virtual
+{
+    partial class principal_panel
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(principal_panel));
+            this.record_button = new System.Windows.Forms.Button();
+            this.recordTxt_txtBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // record_button
+            // 
+            this.record_button.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.record_button.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("record_button.BackgroundImage")));
+            this.record_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.record_button.Location = new System.Drawing.Point(332, 164);
+            this.record_button.Name = "record_button";
+            this.record_button.Size = new System.Drawing.Size(132, 166);
+            this.record_button.TabIndex = 0;
+            this.record_button.UseVisualStyleBackColor = false;
+            this.record_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // recordTxt_txtBox
+            // 
+            this.recordTxt_txtBox.Font = new System.Drawing.Font("Segoe Print", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.recordTxt_txtBox.Location = new System.Drawing.Point(248, 336);
+            this.recordTxt_txtBox.Multiline = true;
+            this.recordTxt_txtBox.Name = "recordTxt_txtBox";
+            this.recordTxt_txtBox.Size = new System.Drawing.Size(310, 130);
+            this.recordTxt_txtBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Viner Hand ITC", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(71, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(698, 129);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Asistente Virtual";
+            // 
+            // principal_panel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.recordTxt_txtBox);
+            this.Controls.Add(this.record_button);
+            this.Name = "principal_panel";
+            this.Size = new System.Drawing.Size(806, 636);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button record_button;
+        private System.Windows.Forms.TextBox recordTxt_txtBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
