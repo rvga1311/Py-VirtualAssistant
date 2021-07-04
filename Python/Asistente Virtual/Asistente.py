fd01580d5797cf584bdb973672898d782ef71803
Lanzerth <rvga1311@gmail.com>
2021-07-03 22:33:31 -0600

    update

--- Python/Asistente Virtual/Asistente.py
+++ Python/Asistente Virtual/Asistente.py
@@ -1,50 +1,108 @@
 import speech_recognition as sr
-import pyttsx3, pywhatkit, datetime
+import pyttsx3, pywhatkit, datetime, os, winshell
 from tkinter import *
+from tkinter import filedialog, messagebox
 from threading import Thread
+from pytube import YouTube
+
+def voiceAssistant(engine,phrase):
+    engine.say(phrase)
+    engine.runAndWait()
+
 
 def assistant(engine, listener, txtBox):
     saludo = "Que deseas que haga por ti, jefe"
-    thread = Thread(target=voiceAsistente, args=(engine, saludo))
+    thread = Thread(target=voiceAssistant, args=(engine, saludo))
     thread.start()
 
-    with sr.Microphone() as source:
-        print("Escuchando...")
-        rec = listener.adjust_for_ambient_noise(source)
-        voice = listener.listen(source)
-        rec= listener.listen(source)
-        rec= listener.recognize_google(voice, language = "es" )
-        rec =  rec.lower()
+    try:
+        with sr.Microphone() as source:
+            print("Escuchando...")
+            rec = listener.adjust_for_ambient_noise(source)
+            voice = listener.listen(source)
+            rec= listener.listen(source)
+            rec= listener.recognize_google(voice, language = "es" )
+            rec =  rec.lower()
 
-    txtBox.delete('1.0', END)
-    txtBox.insert(END,rec)
+        txtBox.delete('1.0', END)
+        txtBox.insert(END,rec)
 
-    if 'busca en google' in rec:
-        search = rec
-        search =  search.replace('busca en google ','') 
-        frase = "Buscando en google " + search
-        thread.join()
-        thread = Thread(target=voiceAsistente, args=(engine, frase))
-        thread.start()
-        pywhatkit.search(search)
-    elif 'reproduce' in rec:
-        music = rec
-        music =  music.replace('reproduce ','') 
-        frase = "Reproduciendo " + music + ", que lo disfrutes"
-        thread.join()
-        thread = Thread(target=voiceAsistente, args=(engine, frase))
-        thread.start()
-        pywhatkit.playonyt(music)
-    elif 'que hora' in rec or 'qué hora' in rec:
-        hora = datetime.datetime.now().strftime('%H:%M %p')
-        thread = Thread(target=voiceAsistente, args=(engine, "Son las "+ hora))
-        thread.start()
-    else:
-        frase = "Vuelve a intentarlo."
-        thread.join()
-        thread = Thread(target=voiceAsistente, args=(engine, frase))
+        if 'busca' in rec:
+            search = rec
+            search =  search.replace('busca ','')
+            search =  search.replace(' en google','') 
+            phrase = "Buscando en google: " + search
+            thread.join()
+            thread = Thread(target=voiceAssistant, args=(engine, phrase))
+            thread.start()
+            pywhatkit.search(search)
+        elif 'reproduce' in rec:
+            music = rec
+            music =  music.replace('reproduce ','') 
+            phrase = "Reproduciendo: " + music + ", que lo disfrutes"
+            thread.join()
+            thread = Thread(target=voiceAssistant, args=(engine, phrase))
+            thread.start()
+            pywhatkit.playonyt(music)
+        elif 'que hora' in rec or 'qué hora' in rec:
+            hora = datetime.datetime.now().strftime('%H:%M %p')
+            thread.join()
+            thread = Thread(target=voiceAssistant, args=(engine, "Son las "+ hora))
+            thread.start()
+        elif 'abre' in rec:
+            if 'el navegador' in rec:
+                thread.join()
+                thread = Thread(target=voiceAssistant, args=(engine, "Abriendo el navegador, que tengas buenas partidas"))
+                thread.start()
+                os.system('start C:/Users/RVGA1/AppData/Roaming/Microsoft/Windows/"Start Menu"/Programs/"Opera GX".lnk')
+        elif 'cierra' in rec:
+            if 'el navegador' in rec:
+                thread.join()
+                thread = Thread(target=voiceAssistant, args=(engine, "Cerrando el navegador..."))
+                thread.start()
+                os.system('taskkill /F /IM opera.exe')
+        elif 'la papelera' in rec:
+            thread.join()
+            thread = Thread(target=voiceAssistant, args=(engine, "Vaciando la papelera de reciclaje"))
+            thread.start()
+            winshell.recycle_bin().empty(confirm=False, show_progress=False, sound=False)
+        else:
+            frase = "Vuelve a intentarlo."
+            thread.join()
+            thread = Thread(target=voiceAssistant, args=(engine, frase))
+            thread.start()
+    except:
+        thread = Thread(target=voiceAssistant, args=(engine, "Ha ocurrido un error, vuelve a intentarlo."))
         thread.start()
 
-def voiceAsistente(engine,frase):
-    engine.say(frase)
-    engine.runAndWait()
+
+link_video = ""
+    
+
+#donwload video
+def DownloadVideo(label, choices, choices_combBox, link_txtbox,Folder_Name):
+    choice = choices_combBox.get()
+
+    if(len(link_video)>1):
+        label.config(text="")
+        yt = YouTube(link_video)
+
+        if(choice == choices[0]):
+            select = yt.streams.filter(progressive=True).first()
+
+        elif(choice == choices[1]):
+            select = yt.streams.filter(progressive=True,file_extension='mp4').last()
+
+        elif(choice == choices[2]):
+            select = yt.streams.filter(only_audio=True).first()
+
+        else:
+            label.config(text="Paste Link again!!",fg="red")
+
+    #download function
+    try:
+        select.download(Folder_Name)
+        label.config(text="Download Completed!!")
+    except:
+        messagebox.showerror("Error","La descarga no se ha podido ejecutar de manera correcta.")
+
--- Python/Asistente Virtual/main.py
+++ Python/Asistente Virtual/main.py
@@ -1,43 +1,178 @@
+# ============================== Librerias ============================== #
 from tkinter import *
+from tkinter import filedialog, ttk, messagebox
 from PIL import Image, ImageTk
 import speech_recognition as sr
-import pyttsx3
+import pyttsx3, os
 from threading import Thread
 from Asistente import assistant
+from pytube import YouTube
 
+#=================================================================================
+# ============================== inicializaciones ============================== #
 # =============== reconocimiento de voz =============== #
 listener = sr.Recognizer()
-
 # =============== voz del asistente =============== #
 engine = pyttsx3.init()
 engine.setProperty('voice', engine.getProperty('voices')[3].id) # eleccion de voz
 engine.setProperty('rate', 160) # velocidad
 
-def thing():
-    thread1 = Thread(target=assistant, args=(engine, listener, record_txtBox))
-    thread1.start()
+#==========================================================================
+# ============================== Funciones ============================== #
+def virtualAssitant_func():
+    thread = Thread(target=assistant, args=(engine, listener, record_txtBox))
+    thread.start()
 
-# =============== Ventana principal =============== #
+Folder_Name = ""
+#file location
+def openLocation():
+    global Folder_Name
+    Folder_Name = filedialog.askdirectory()
+    if(len(Folder_Name) > 1):
+        file = Folder_Name
+        if len(Folder_Name) > 36:
+            file = ".../"+ os.path.basename(file)
+        path_lbl.config(text=file,fg="green")
+
+    else:
+        path_lbl.config(text="Porfavor, seleccione una direccion valida.",fg="red")
+
+def upload():
+    url = link_entry.get()
+    if(len(url)>1):
+        yt = YouTube(url)
+def ytDownload():
+    print("a")
+
+
+
+#==================================================================================
+# ============================== Ventana principal ============================== #
 window = Tk()
 window.title("Asistente virtual")
-window.geometry('600x500')
+window.geometry('1000x700')
 window.iconbitmap('D:/Programacion/Proyectos Personales/Python/Asistente Virtual/Media/robotica.ico')
+window.resizable(False, False)
 
+fontWindow = ("Segoe Print", 12)
 
-# =============== Boton para reconocimiento de voz =============== #
+#==================================================================
+# =============== Frame Asistente Virtual por voz =============== #
+virtualAssitant_frame =  Frame(window, width=750, height=700)
+
+# Label titulo
+title_lbl = Label(virtualAssitant_frame, text="Asistente Virtual", font=("Segoe Print", 24))
+title_lbl.pack(pady=20, padx= 100, side=TOP)
+
+# Boton para reconocimiento de voz
 img = Image.open("D:/Programacion/Proyectos Personales/Python/Asistente Virtual/Media/microphone.png")
 size = 100
 new_img = img.resize((size,size))
 imagetest = ImageTk.PhotoImage(new_img)
-record_btn = Button(window, image=imagetest, command=thing, height = size, width = size, borderwidth=5, bg='#32a6a8')
-record_btn.pack(pady=20)
+record_btn = Button(virtualAssitant_frame, image=imagetest, command=virtualAssitant_func, height = size, width = size, borderwidth=5, bg='#32a6a8')
+record_btn.pack(pady=20, padx= 100, side=TOP)
 
-# =============== TextBox para reconocimiento de voz =============== #
+# TextBox para reconocimiento de voz
 size = 12
-record_txtBox = Text(window, height= size, width= size*3)
-record_txtBox.pack(pady=20)
+record_txtBox = Text(virtualAssitant_frame, height= size, width= size*3)
+record_txtBox.pack(pady=20, padx= 100, side=TOP)
 
+#===========================================================
+# =============== Frame Youtube Downloader =============== #
+ytDownloader_frame =  Frame(window, width=750, height=700, bg="green")
+# Label titulo
+title_lbl2 = Label(ytDownloader_frame, text="Youtube Downloader", font=("Segoe Print", 24))
+title_lbl2.pack(pady=20, padx= 10, side=TOP)
+
+#================================================ #
+link_frame = Frame(ytDownloader_frame, bg="red")
+link_frame.pack(side=TOP)
+
+#Label Link
+link_lbl = Label(link_frame,text="Link:",font=("Segoe Print",15))
+link_lbl.pack(pady=20, padx= 10, side=LEFT)
+
+#TexBox link
+link_entryVar = StringVar()
+link_entry = Entry(link_frame,width=20,textvariable=link_entryVar, font=("Segoe Print",13))
+link_entry.pack(pady=20, padx= 10, side=LEFT)
+
+#upload path
+upload_button = Button(link_frame,text="Cargar",font=("Segoe Print",15), command=upload, borderwidth=2, bg='#32a6a8')
+upload_button.pack(padx= 10, side=LEFT)
+
+#================================================ #
+path_frame = Frame(ytDownloader_frame, bg="blue")
+path_frame.pack(side=LEFT)
+
+#Button path
+path_button = Button(path_frame,text="Folder:",font=("Segoe Print",15), command=openLocation, borderwidth=4, bg='#32a6a8')
+path_button.pack(pady=20, padx= 10, side=LEFT)
+
+#label path
+path_lbl = Label(path_frame,font=("Segoe Print",12))
+path_lbl.pack(pady=20, padx= 10, side=LEFT)
+
+#================================================ #
+quality_frame = Frame(ytDownloader_frame, bg="red")
+quality_frame.pack(side=LEFT)
+
+#Label quality
+quality_lbl = Label(quality_frame,text="Calidad:",font=("Segoe Print",15))
+quality_lbl.pack(pady=20, padx= 10, side=LEFT)
+
+#combobox calidad
+ytdchoices_combBox = ttk.Combobox(quality_frame)
+ytdchoices_combBox.pack(pady=20, padx= 10, side=LEFT)
+
+
+#===================================================
+# =============== Frame txt_Speech =============== #
+txt_Speech_frame =  Frame(window, width=750, height=700)
+
+
+
+#===============================================================
+#=============== funciones para dibujar frames =============== #
+def draw_virtualAssitant_frame(): # =============== Frame Asistente Virtual por voz =============== #
+    ytDownloader_frame.pack_forget()
+    txt_Speech_frame.pack_forget()
+    virtualAssitant_frame.pack(side=LEFT)    
+
+def draw_ytDownloader_frame(): # =============== Frame Youtube Downloader =============== #
+    virtualAssitant_frame.pack_forget()
+    txt_Speech_frame.pack_forget()
+    ytDownloader_frame.pack(side=LEFT, padx=50)
+
+def draw_txt_Speech_frame(): # =============== Frame txt_Speech =============== #
+    virtualAssitant_frame.pack_forget()
+    ytDownloader_frame.pack_forget()
+    txt_Speech_frame.pack(side=LEFT)
+
+#=============================================
+# =============== Frame Menu =============== #
+spacing = 10
+#Frame
+menu_frame =  Frame(window, width=250, height=700)
+menu_frame.pack(side=LEFT)
+
+#boton del frame asistente de voz
+virtualAsstitant_btn = Button(menu_frame, text="Asistente Virtual por voz", command=draw_virtualAssitant_frame, borderwidth=4, bg='#32a6a8', font=fontWindow)
+virtualAsstitant_btn.pack(side=TOP,pady=spacing, padx=spacing)
+
+#boton del frame Youtube Downloader
+ytDownloader_btn = Button(menu_frame, text="Youtube Downloader", command=draw_ytDownloader_frame, borderwidth=4, bg='#32a6a8', font=fontWindow)
+ytDownloader_btn.pack(side=TOP,pady=spacing, padx=spacing)
+
+#boton del frame Text <=> Speech
+txt_Speech = Button(menu_frame, text="Text <=> Speech", command=draw_txt_Speech_frame, borderwidth=4, bg='#32a6a8', font=fontWindow)
+txt_Speech.pack(side=TOP,pady=spacing, padx=spacing)
+
+
+
+#=================================================
 # =============== Loop principal =============== #
+draw_virtualAssitant_frame()
 window.mainloop()
 
 
